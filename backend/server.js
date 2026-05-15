c540a4feb99add424446a9a7f71e98f84adcf300
Emandoyesus Tesfay <emandoyesus@gmail.com>
2026-05-15 07:03:18 +0300

    finalize

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the AXORA Movies API. All endpoints are under /api');
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/watchlist', watchlistRoutes);
 
